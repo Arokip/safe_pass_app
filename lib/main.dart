62af79113dca3cf6a9b697f114a106c2bda78311
Jindra <jindrich.pikora@gmail.com>
2021-10-13 17:55:57 +0200

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:safe_pass_app/screen/detail.dart';
+import 'package:safe_pass_app/screen/form.dart';
+import 'package:safe_pass_app/screen/main.dart';
 
 void main() {
+  //
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Safe Password application',
+      initialRoute: '/main',
+      routes: {
+        '/main': (context) => const MainScreen(),
+        '/detail': (context) => const DetailScreen(),
+        '/form': (context) => const FormScreen(),
+      },
       home: Container(),
     );
   }
